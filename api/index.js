fe3d36dd01e34e1132484af457f3cbfc95242a39
PoseiDon <suriya.b@kkumail.com>
2024-12-24 20:13:12 +0700

    fixed เพิ่ม index.html

--- api/index.js
+++ api/index.js
@@ -4,12 +4,14 @@ const PORT = 4000;
 
 // เส้นทางหลัก
 app.get('/', (req, res) => {
-    res.send('บักโง่โดนกูหลอก แต่ "/about" อาจจะมีอะไรเจ่งๆ');
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.get('/about', (req, res) => {
     res.send('มึงนี้มันโง่จริงๆ');
 });
+
+
 
 // ส่งออกฟังก์ชันเพื่อให้ Vercel ใช้งาน
 module.exports = (req, res) => {
