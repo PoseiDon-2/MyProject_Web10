a0835cdab607a7a79a3b268c05c0fbac45ef54fd
PoseiDon <suriya.b@kkumail.com>
2024-12-28 17:58:54 +0700

    แก้ไข เพิ่มการตรวจสอบว่ามีผู้ใช้งานนี้แล้ว7

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,12 +1,18 @@
-const User = require('../models/User');
+module.exports = (req, res) => {
 
-module.exports = async (req, res) => {
     let email = "";
     let password = "";
     let data = req.flash('data')[0];
 
-    if (typeof data != "undefined") {
-        email = data.email;
-        password = data.password;
-    }
-};
+if (typeof data != "undefined") {
+    email = data.email;
+    password = data.password;
+
+}
+
+    res.render('register',{
+        errors: req.flash('validationErrors'),
+        email: email,
+        password: password
+    })
+}
