20c2c401108e52ddc6c72fa04e727baf39fb3217
PoseiDon <suriya.b@kkumail.com>
2024-12-28 17:51:30 +0700

    แก้ไข เพิ่มการตรวจสอบว่ามีผู้ใช้งานนี้แล้ว4

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,26 +1,18 @@
-const User = require('../models/User');
+module.exports = (req, res) => {
 
-module.exports = async (req, res) => {
     let email = "";
     let password = "";
     let data = req.flash('data')[0];
 
-    if (typeof data != "undefined") {
-        email = data.email;
-        password = data.password;
-    }
+if (typeof data != "undefined") {
+    email = data.email;
+    password = data.password;
 
-    // ตรวจสอบว่ามีผู้ใช้งานนี้อยู่ในระบบแล้วหรือไม่
-    const existingUser = await User.findOne({ email: email });
-    if (existingUser) {
-        req.flash('validationErrors', ['Email is already in use']);
-        req.flash('data', { email, password });
-        return res.redirect('/register');
-    }
+}
 
-    res.render('register', {
+    res.render('register',{
         errors: req.flash('validationErrors'),
         email: email,
         password: password
-    });
-};
+    })
+}
